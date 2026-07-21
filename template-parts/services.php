97651f41cd5429d96ace7a1800d4ab3fe6083005
Petar Stankovic <petar.stankovic@craterstudio.com>
2026-07-21 23:39:23 +0200

    adding styles to new page

--- template-parts/services.php
+++ template-parts/services.php
@@ -1,21 +1,112 @@
+<?php
+/**
+ * Services Section
+ *
+ * @package LimPlus
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+
 <section class="section services">
 
-    <div class="container">
+	<div class="container">
 
-        <div class="section-heading">
+		<div class="section-heading">
 
-            ...
+			<span class="section-tag">
+				Naše usluge
+			</span>
 
-        </div>
+			<h2 class="section-title">
+				Kompletna rešenja za vaš objekat
+			</h2>
 
-        <div class="services__grid">
+			<p class="section-description">
+				Pružamo kompletnu uslugu od savetovanja i merenja do završnih
+				radova. Radimo kvalitetno, pouzdano i u dogovorenom roku.
+			</p>
 
-            <article class="service-card">
+		</div>
 
-            </article>
+		<div class="services__grid">
 
-        </div>
+			<article class="service-card card">
 
-    </div>
+				<div class="service-card__icon">
+					🏠
+				</div>
+
+				<h3 class="service-card__title">
+					Krovovi
+				</h3>
+
+				<p class="service-card__description">
+					Izrada novih i rekonstrukcija postojećih krovova svih tipova.
+				</p>
+
+				<ul class="service-card__list">
+
+					<li>Novi krovovi</li>
+					<li>Rekonstrukcija</li>
+					<li>Sanacija</li>
+
+				</ul>
+
+			</article>
+
+			<article class="service-card card">
+
+				<div class="service-card__icon">
+					🛠
+				</div>
+
+				<h3 class="service-card__title">
+					Limarski radovi
+				</h3>
+
+				<p class="service-card__description">
+					Izrada i montaža svih vrsta limarskih elemenata.
+				</p>
+
+				<ul class="service-card__list">
+
+					<li>Oluci</li>
+					<li>Opšivke</li>
+					<li>Vetar lajsne</li>
+
+				</ul>
+
+			</article>
+
+			<article class="service-card card">
+
+				<div class="service-card__icon">
+					🧱
+				</div>
+
+				<h3 class="service-card__title">
+					Fasade
+				</h3>
+
+				<p class="service-card__description">
+					Izrada termo i dekorativnih fasada za stambene i poslovne objekte.
+				</p>
+
+				<ul class="service-card__list">
+
+					<li>Termo fasade</li>
+					<li>Dekorativne fasade</li>
+					<li>Sanacija fasada</li>
+
+				</ul>
+
+			</article>
+
+		</div>
+
+	</div>
 
 </section>
